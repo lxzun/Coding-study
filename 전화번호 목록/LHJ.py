5258c1c54c6a9c502265b1be4aad6a5371aa2186
LxzHome <rico314@naver.com>
2021-11-14 21:58:45 +0900

    update

--- 전화번호 목록/LHJ.py
+++ 전화번호 목록/LHJ.py
@@ -1,13 +1,14 @@
 def solution(phone_book):
-    pb = {k:[] for k in range(1, 21)}
+    pb = {k: {j: [] for j in range(1, 21)} for k in range(0, 10)}
     for pn in phone_book:
-        pb[len(pn)].append(pn)
-        
-    for i in range(1, 20):
-        for j in pb[i]:
-            for k in range(i+1, 21):
-                for y in pb[k]:
-                    if j == y[:i]:
-                        return False
-    
+        pb[int(pn[0])][len(pn)].append(pn)
+
+    for x in range(0, 10):
+        for i in range(1, 20):
+            for j in pb[x][i]:
+                for k in range(i + 1, 21):
+                    for y in pb[x][k]:
+                        if j == y[:i]:
+                            return False
+
     return True
