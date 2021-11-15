4accdb0c5dbc9b34eb71eca84c9916c25059b373
LxzHome <rico314@naver.com>
2021-11-15 15:14:20 +0900

    upload LXZ

--- 전화번호 목록/LHJ.py
+++ 전화번호 목록/LHJ.py
@@ -1,33 +1,3 @@
-
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
-38
-39
-40
-41
-42
-43
-44
 def solution(phone_book):
     pb = {i:{j:{k:{y:[] for y in range(10)} for k in range(10)} for j in range(10)} for i in range(3, 21)}
     pb[1] = {i:[] for i in range(10)} 
