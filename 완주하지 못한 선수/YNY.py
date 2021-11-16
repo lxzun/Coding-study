6bb99ccbd48745d6620e7bee764385a847b19d21
Nayoung1124 <53847442+Nayoung1124@users.noreply.github.com>
2021-11-16 22:50:00 +0900

    Create YNY.py

---
+++ 완주하지 못한 선수/YNY.py
@@ -0,0 +1,11 @@
+import ast
+def solution(participant, completion):
+    answer = ''
+    participant.sort()
+    completion.sort()
+    completion.append('_')
+    for i,j in zip(participant,completion):
+        if i != j:
+            answer = i
+            break
+    return answer
